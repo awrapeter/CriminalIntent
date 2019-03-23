6db8f00374827bc647f0111e074524281f82a4d6
Peter Awra <peter.awra@gmail.com>
2019-03-23 18:15:43 +0300

    fragmentCrime addCode

--- app/src/main/java/com/study/awra/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/study/awra/criminalintent/CrimeFragment.java
@@ -7,6 +7,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
 
@@ -19,6 +22,9 @@ public class CrimeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_crime,container,false);
+        EditText editText=view.findViewById(R.id.tv_title);
+        Button button =view.findViewById(R.id.bt_detail);
+        CheckBox checkBox =view.findViewById(R.id.cb_solved);
         return view;
     }
 }
