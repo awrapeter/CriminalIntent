efe38b95ac587d054aa23be99ab452b0988380e8
Peter Awra <peter.awra@gmail.com>
2019-03-23 17:31:06 +0300

    add Crime class

---
+++ app/src/main/java/com/study/awra/criminalintent/Crime.java
@@ -0,0 +1,43 @@
+package com.study.awra.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+        mId =UUID.randomUUID() ;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+}
